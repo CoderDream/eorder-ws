48ed26037d0ee12a07d6596a87648810bf8ea301
CoderDream <coderdream@gmail.com>
2014-12-12 14:25:06 +0800

    更新数据库和菜品图片

--- src/test/java/com/innovaee/eorder/dao/UserDaoTest.java
+++ src/test/java/com/innovaee/eorder/dao/UserDaoTest.java
@@ -1,7 +1,5 @@
 package com.innovaee.eorder.dao;
 
-import static org.junit.Assert.fail;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -34,10 +32,18 @@ public class UserDaoTest {
 	}
 
 	@Test
-	public void getUserByCellphone() {
+	public void getUserByCellphone_01() {
 		String cellphone = "13912345673";
 		User user = userDao.getUserByCellphone(cellphone);
 		Assert.assertNotNull(user);
 		Assert.assertEquals("abc", user.getUserName());
+	}
+	
+	@Test
+	public void getUserByCellphone_02() {
+		String cellphone = "13912340004";
+		User user = userDao.getUserByCellphone(cellphone);
+		Assert.assertNotNull(user);
+		Assert.assertEquals("赵六", user.getUserName());
 	}
 }
